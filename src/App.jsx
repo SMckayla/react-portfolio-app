eab9c9a71d39128f36080d8fcd92bfbb7a671017
Sascha Mckayla <mckaylasascha@gmail.com>
2026-05-08 02:28:35 +0300

    setup: basic App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import ProjectCard from "./components/ProjectCard";
-
 function App() {
   return (
     <div>
       <h1>My Portfolio App</h1>
-      <ProjectCard />
     </div>
   );
 }
 
 export default App;
-
-const projects = [
-  {
-    id: 1,
-    title: "Portfolio Website",
-    description: "Built using React"
-  },
-  {
-    id: 2,
-    title: "E-commerce App",
-    description: "Online shopping platform"
-  },
-];
